61b8a939f23dcd6436b2115cbba564c29ba71075
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-06-11 15:10:11 -0400

    feat: Remove redundant if statements from MakeNegative

--- makeNegative.js
+++ makeNegative.js
@@ -1,12 +1,28 @@
 function makeNegative(num) {
   if (num > 0) {
-    num = num * -1;
+    
+    return -num
+  }
+  else {
     return num
   }
-  if (num < 0) {
-    return num
-  }
-  if (num === 0) {
-    return num
-  }
+  
+}
+
+makeNegative(5);
+
+//Best Practices Answers:
+//They return a -0 if the number is 0. 
+
+//1
+function makeNegative(num) {
+  return -Math.abs(num);
+}
+//2
+makeNegative = n => -Math.abs(n)
+
+//3
+
+function makeNegative(num) {
+  return num < 0 ? num : -num;
 }
