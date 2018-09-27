a440f6a67685a0ef57668d2e3ef931ebe5ff3000
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-09-27 17:42:56 -0400

    doc: Remove comments

--- test.js
+++ test.js
@@ -1,7 +1,3 @@
-//==================//
-// 		Part 1	 	//
-//==================//
-
 var userBalance = 0;
 var lastClick = 0;
 var timeArray = [0];
@@ -24,21 +20,3 @@ function onUpdate({ delta }) {
   lastClick += delta;
 }
 
-// You have access to a function updateBalance which
-// takes in a string and sets the ui to that value
-// updateBalance(userBalance);
-
-//==================//
-// 		Part 2	 	//
-//==================//
-
-function processSlots(input) {
-  console.log("Output: ");
-}
-
-// examples input
-var array = [[1, 0, 0, 0, 1], [0, 1, 0, 1, 0], [0, 0, 1, 0, 0]];
-//Output: '1 winning line, scoring 20 points.'
-
-var array2 = [[2, 4, 2, 2, 3], [1, 1, 1, 4, 1], [3, 3, 3, 4, 2]];
-//Output: '2 winning lines, scoring a total 30 points.'
