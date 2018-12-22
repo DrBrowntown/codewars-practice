681af2718ee01f15dc7c32535663192fa35c1429
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-12-21 23:15:40 -0500

    feat: Add math operations

---
+++ basic-mathmatical-operations.js
@@ -0,0 +1,25 @@
+// The function should take three arguments -
+// operation(string/char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+
+function basicOp(operation, value1, value2) {
+  if (operation === "+") {
+    return value1 + value2;
+  }
+  if (operation === "-") {
+    return value1 - value2;
+  }
+  if (operation === "*") {
+    return value1 * value2;
+  }
+  if (operation === "/") {
+    return value1 / value2;
+  } else {
+    console.log("Invalid operator");
+  }
+}
+
+basicOp("+", 4, 7); // Output: 11
+basicOp("-", 15, 18); // Output: -3
+basicOp("*", 5, 5); // Output: 25
+basicOp("/", 49, 7); // Output: 7
