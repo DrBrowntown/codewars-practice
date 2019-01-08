fed86cf647075311fd8664cd4b1891c302bfb548
Calvin Brownlee <calvinbrownlee@gmail.com>
2019-01-08 16:46:07 -0500

    feat: Another basic math operation

--- basic-mathmatical-operations.js
+++ basic-mathmatical-operations.js
@@ -23,3 +23,20 @@ basicOp("+", 4, 7); // Output: 11
 basicOp("-", 15, 18); // Output: -3
 basicOp("*", 5, 5); // Output: 25
 basicOp("/", 49, 7); // Output: 7
+
+function arithmetic(value1, value2, operation) {
+  if (operation === "add") {
+    return value1 + value2;
+  }
+  if (operation === "subtract") {
+    return value1 - value2;
+  }
+  if (operation === "multiply") {
+    return value1 * value2;
+  }
+  if (operation === "divide") {
+    return value1 / value2;
+  } else {
+    console.log("Invalid operator");
+  }
+}
